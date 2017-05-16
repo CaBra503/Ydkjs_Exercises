e437de05eec0bc229aaa73a8e368b51f4c2ed60c
CaBra503 <Cabranze@gmail.com>
2017-05-15 21:48:57 -0700

    Break infinte while loop & Added var afterTax

--- chp1Practise.js
+++ chp1Practise.js
@@ -1,57 +1,46 @@
-/*
-    Program calculates the cost of a sale. Takes cost of purchase tests to see
-    if the funds are available.
-*/
+// Defining constant variables.
 const TAX_RATE = 0.08;
 const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 09.99;
-const SPENDING_LIMIT = promp('How much are you lookin to spend?', '200?');
-// Bonus credit: User imput via prompt.
+const SPENDING_LIMIT = prompt('How much are you lookin to spend?', '200?');
 
+//Defining variables.
 var initialBalance = prompt('What is the current amount of your bank account?', 'over9000');
-var accountBalance = initialBalance;
-var purchaseAmount;
+var accountBalance = Number.parseInt(initialBalance);
+var amount;
+var tempSalesAmount;
 var salesAmountFinal;
 
 //funtion definitions.
-function calculateAfterTax(){
-  amtTax = amtTax + (purchaseAmount * TAX_RATE);
-    return amtTax;
+function calculateAfterTax() {
+  var  amountAfterTax = amountAfterTax + (tempSalesAmount * TAX_RATE);
+    return amountAfterTax;
 }
 
-function formatAmount(amt){
-  return '$' + amt.toFixed(2);
+function formatAmount(amount) {
+    return '$' + amount.toFixed(2);
 }
 
-/*  ToDo implement loop and functions 5/4
-5/8
-*/
-while (accountBalance > purchaseAmount){
-  console.log("Your initial balance is " + formatAmount(accountBalance));
-  purchaseAmount = PHONE_PRICE;
-   console.log("The phone youre after is " + formatAmount(purchaseAmount));
+// Logic  and conditionals.
+console.log('Your Bank balance is ' + formatAmount(accountBalance));
+while (accountBalance > SPENDING_LIMIT) {
+    tempSalesAmount = tempSalesAmount + PHONE_PRICE;
+     if (accountBalance > tempSalesAmount) {
+      tempSalesAmount = tempSalesAmount + PHONE_PRICE + ACCESORIES;
+      accountBalance = accountBalance - tempSalesAmount;
+      console.log('after adding the accesory to your order, your current balance is ' + formatAmount(accountBalance));
 
-  if (accountBalance > purchaseAmount){
-  	console.log("Funds available");
-  	if (accountBalance > purchaseAmount){
-  		accountBalance = accountBalance - purchaseAmount;
-  console.log("Youve bought the phone, your current balance is " + formatAmount(accountBalance));
+    }
+      else (accountBalance < tempSalesAmount)
+        console.log('Im sorry the but your card has been declined');
+        console.log('currentBalance is ' + formatAmount(accountBalance));
 
-  	}
-
-    console.log('How about some Accesories?')
-  	purchaseAmount = purchaseAmount + ACCESORIES;
-  	if (accountBalance > purchaseAmount){
-  		accountBalance = accountBalance - purchaseAmount;
-  console.log("after adding the accesory to your purchase, your total is " + formatAmount(accountBalance));
-  	}
-  } else if (accountBalance < purchaseAmount){
-  	console.log("Im sorry the funds arent available");
-  console.log("currentBalance is " + formatAmount(accountBalance));
-
-  }
-
+      break;
 }
+
+    console.log(calculateAfterTax(tempSalesAmount));
+
+
 
 
 
